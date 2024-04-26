7afd836f980d083b48e059c5510b62c8cb3809a5
YanaY27 <yanayeromina@gmail.com>
2024-04-26 09:00:39 +0300

    upd

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// import './App.css'
+
+// function App() {
+
+//   return (
+//     <>
+
+//     </>
+//   )
+// }
+
+// export default App
+import userData from "./userData.json";
+import Profile from "./components/Profile/Profile";
+
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+};
+
+export default App;
